bda0be567960b8b519b3ed6a86546ad055173c7a
mitsu1119 <hachimitsu1119@gmail.com>
2024-12-03 13:13:47 +0900

    bytesでテストできるようにした

--- src/cavp_test.rs
+++ src/cavp_test.rs
@@ -21,6 +21,13 @@ pub struct ShaTriData {
     md: String,
 }
 
+#[derive(Debug)]
+pub struct ShaTriDataBytes {
+    pub bit_len: u32,
+    pub msg: Vec<u8>,
+    md: Vec<u8>,
+}
+
 impl ShaTriData {
     fn new(bit_len: u32, msg: String, md: String) -> Self {
         Self { bit_len, msg, md }
@@ -31,6 +38,34 @@ impl ShaTriData {
             Ok(())
         } else {
             Err(CavpError::TestFailed(self.msg.clone()))
+        }
+    }
+
+    pub fn as_bytes(&self) -> ShaTriDataBytes {
+        let bytes_msg = (0..self.msg.len())
+            .step_by(2)
+            .map(|i| u8::from_str_radix(&self.msg[i..i + 2], 16).unwrap())
+            .collect();
+
+        let bytes_md = (0..self.md.len())
+            .step_by(2)
+            .map(|i| u8::from_str_radix(&self.md[i..i + 2], 16).unwrap())
+            .collect();
+
+        ShaTriDataBytes::new(self.bit_len, bytes_msg, bytes_md)
+    }
+}
+
+impl ShaTriDataBytes {
+    fn new(bit_len: u32, msg: Vec<u8>, md: Vec<u8>) -> Self {
+        Self { bit_len, msg, md }
+    }
+
+    pub fn test(&self, md: Vec<u8>) -> Result<()> {
+        if md == self.md {
+            Ok(())
+        } else {
+            Err(CavpError::TestFailed(format!("{:x?}", self.msg)))
         }
     }
 }
