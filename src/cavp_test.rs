057f039b9d2c2b83eaa9be98ea7fdf81b65abb74
mitsu1119 <hachimitsu1119@gmail.com>
2024-12-06 16:16:20 +0900

    sha384のテストを実装

--- src/cavp_test.rs
+++ src/cavp_test.rs
@@ -134,4 +134,15 @@ impl<'a> CavpTest<'a> {
 
         Ok(short_msgs)
     }
+
+    pub fn sha384_byte_testvectors(&self) -> Result<Vec<ShaTriData>> {
+        let sha_root = Path::new("shabytetestvectors");
+
+        let mut short_msgs = self.tri_parse(&sha_root.join("SHA384ShortMsg.rsp"))?;
+        let long_msgs = self.tri_parse(&sha_root.join("SHA384LongMsg.rsp"))?;
+
+        short_msgs.extend(long_msgs);
+
+        Ok(short_msgs)
+    }
 }
